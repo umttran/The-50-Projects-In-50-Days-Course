8c3a1885087f2a1a6bdb1b8f46781cca9b6d4d4e
Umut Turan <umutturan107@gmail.com>
2022-02-10 17:32:17 +0300

    Add event listener to buttons

--- Day-2-Progress-Steps/script.js
+++ Day-2-Progress-Steps/script.js
@@ -5,4 +5,23 @@ const nextBtn = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
 
 // The variable will be used to know which step is active(like an index) 
-let currentActive = 1
+let currentActive = 1;
+
+nextBtn.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+})
+
+prevBtn.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+})
+
