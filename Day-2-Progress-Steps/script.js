38aac01f4eb0375c347e64130cdf043165c4a1df
Umut Turan <umutturan107@gmail.com>
2022-02-11 10:59:13 +0300

    Add status update for buttons - End of Project

--- Day-2-Progress-Steps/script.js
+++ Day-2-Progress-Steps/script.js
@@ -25,7 +25,7 @@ prevBtn.addEventListener("click", () => {
   update();
 })
 
-// Function to update step circles status & progress bar
+// Function to update status of step circles, progress bar & buttons
 function update() {
   // Circle status
   circles.forEach((circle, index) => {
@@ -44,4 +44,15 @@ function update() {
   // Course solution:
   // const activeCircles = document.querySelectorAll(".active");
   // progress.style.width = (activeCircles.length - 1) / (circles.length - 1) * 100 + "%";
+
+  // Button Status
+  if (currentActive === 1) {
+    prevBtn.disabled = true;
+  } else if (currentActive === circles.length) {
+    nextBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+  
 }
