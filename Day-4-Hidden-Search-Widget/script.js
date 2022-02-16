423cfcf1d9e08e9098be33233c6306cfd58137de
Umut Turan <umutturan107@gmail.com>
2022-02-16 10:37:14 +0300

    Add functionality with JS

---
+++ Day-4-Hidden-Search-Widget/script.js
@@ -0,0 +1,9 @@
+// Create variables to bring elements from the DOM
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+})
