1187b80e21d283914a4879645d7e743275ba4175
Umut Turan <umutturan107@gmail.com>
2022-02-28 11:04:05 +0300

    Edit JS for wave effect

--- Day-8-Form-Wave-Animation/script.js
+++ Day-8-Form-Wave-Animation/script.js
@@ -5,9 +5,11 @@ const labels = document.querySelectorAll(".form-control label");
 // 1- split() will seperate label letters into an array
 // 2- map() will create the array with the letters placed in the span tag
 // 3- join() will turn array back into a string
+// 4- transition-delay for wave effect
 labels.forEach(label => {
   label.innerHTML = label.innerText
     .split("")
-    .map((eachLetter) => `<span>${eachLetter}</span>`)
+    .map((eachLetter, idx) => 
+      `<span style="transition-delay:${idx * 60}ms">${eachLetter}</span>`)
     .join("");
 })
