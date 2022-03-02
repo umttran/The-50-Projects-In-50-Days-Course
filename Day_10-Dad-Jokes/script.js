f37aa2a919f9659dd93fc472d21c6bae1993f11d
Umut Turan <umutturan107@gmail.com>
2022-03-02 08:28:43 +0300

    Add random joke generation via Fetch API

---
+++ Day_10-Dad-Jokes/script.js
@@ -0,0 +1,23 @@
+// Create variables to bring elements from the DOM
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+// To get another joke when button is clicked 
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke();
+
+// Generate random joke using with Fetch API 
+function generateJoke() {
+  const config = {
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+
+  fetch('https://icanhazdadjoke.com', config)
+  .then(res => res.json())
+  .then(data => {
+    jokeEl.innerHTML = data.joke;
+  });
+}
