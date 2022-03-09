5c91e1048755807362bde1ee149fc06ef9fdae75
Umut Turan <umutturan107@gmail.com>
2022-03-09 11:59:51 +0300

    Add JS to empty back the last selected cup

--- Day_16-Drink-Water/script.js
+++ Day_16-Drink-Water/script.js
@@ -9,8 +9,14 @@ smallCups.forEach((cup, index) => {
   cup.addEventListener('click', () => highlightCups(index))
 })
 
-// Highlight function - index is selectedIndex for the function.
+// Highlight function - index become selected cup index for the function
 function highlightCups(selectedIndex) {
+  // If user selects a cup and clicks it again, empty that cup back
+  if(smallCups[selectedIndex].classList.contains('full') && 
+  !smallCups[selectedIndex].nextElementSibling?.classList.contains('full')) {
+    selectedIndex--;
+  }
+  
   smallCups.forEach((cup, idx ) => {
     if(idx <= selectedIndex) {
       cup.classList.add('full');
