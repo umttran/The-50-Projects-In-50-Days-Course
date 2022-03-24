a88cb5cad148670ab089d43426a0937b20d4d51c
Umut Turan <umutturan107@gmail.com>
2022-03-24 22:19:02 +0300

    Add event listeners for drawing functionality via JS

--- Day_22-Drawing-App/script.js
+++ Day_22-Drawing-App/script.js
@@ -8,6 +8,39 @@ let size = 10;
 let color = 'black';
 let x;
 let y;
+let isPressed = false; // To check if the mouse is pressed or not
+
+// When the mouse is pressed
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true;
+
+  // Assign the cursor positions
+  x = e.offsetX;
+  y = e.offsetY;
+})
+
+// When the mouse is released
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false;
+
+  // Assign the cursor positions
+  x = undefined;
+  y = undefined;
+})
+
+// When the cursor moves while mouse is pressed
+canvas.addEventListener('mousemove', (e) => {
+  if(isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+})
 
 // Circle Drawing Function
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#:~:text=ctx.arc(75%2C%2075%2C%2050%2C%200%2C%20Math.PI%20*%202%2C%20true)%3B
@@ -28,7 +61,3 @@ function drawLine(x1, y1, x2, y2) {
   // Multiply lineWidth by 2 to make the line the same thickness as the circle
   ctx.stroke();
 }
-
-// Call functions for sample drawings
-drawCircle(300,550);
-drawLine(0 , 50, 270, 500);
