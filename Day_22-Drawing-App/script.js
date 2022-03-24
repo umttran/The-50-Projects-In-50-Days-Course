d00adbe0469371d7c4bb9c1f6ed58db61a27ab10
Umut Turan <umutturan107@gmail.com>
2022-03-24 21:25:52 +0300

    Add JS to get 2D context on canvas element

---
+++ Day_22-Drawing-App/script.js
@@ -0,0 +1,4 @@
+// Bring canvas element from the DOM and create context
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
