b2780ce3f4caefbfdf912a2023c2ebc0273132d5
Umut Turan <umutturan107@gmail.com>
2022-03-30 23:59:58 +0300

    Place the last text in the correct position

--- Day_26-Double-Vertical-Slider/script.js
+++ Day_26-Double-Vertical-Slider/script.js
@@ -4,3 +4,11 @@ const textSlide = document.querySelector('.text-slide');
 const imageSlide = document.querySelector('.image-slide');
 const downBtn = document.querySelector('.down-btn');
 const upBtn = document.querySelector('.up-btn');
+
+// Number of Slides
+const slidesLength = imageSlide.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+// Place the last text in the correct position
+textSlide.style.top = `-${(slidesLength - 1) * 100}vh`;
