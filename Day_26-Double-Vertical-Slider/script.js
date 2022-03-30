3a1d3803321c155664ab1b01a51391a86db00bf2
Umut Turan <umutturan107@gmail.com>
2022-03-30 23:59:19 +0300

    Add JS to bring elements from the DOM

---
+++ Day_26-Double-Vertical-Slider/script.js
@@ -0,0 +1,6 @@
+// Bring elements from the DOM
+const sliderContainer = document.querySelector('.slider-container');
+const textSlide = document.querySelector('.text-slide');
+const imageSlide = document.querySelector('.image-slide');
+const downBtn = document.querySelector('.down-btn');
+const upBtn = document.querySelector('.up-btn');
