40ffbb9eadaaa06f81060a714871c5312e84a9eb
Umut Turan <umutturan107@gmail.com>
2022-03-31 17:19:54 +0300

    Add JS to create notifications with a random type

--- Day_27-Toast-Notification/script.js
+++ Day_27-Toast-Notification/script.js
@@ -2,6 +2,7 @@
 const button = document.getElementById('button');
 const toasts = document.getElementById('toasts');
 
+// Notification Messages
 const messages = [
   'Toast Notification One',
   'Toast Notification Two',
@@ -9,16 +10,26 @@ const messages = [
   'Toast Notification Four',
   'Toast Notification Five',
   'Toast Notification Six'
-]
+];
+
+// Notification Types
+const types = [
+  'info', 'success', 'error', 'warning', 'dark'
+];
 
 // Button Event Listener
 button.addEventListener('click', () => createNotification());
 
+// Function to create new random notification with a random type
 function createNotification() {
   const newNotification = document.createElement('div');
   newNotification.classList.add('toast');
-
+  
+  // Generate random notification message
   newNotification.innerText = getRandomNotification();
+
+  // Give that message a random notification type
+  newNotification.classList.add(getRandomType());
 
   toasts.appendChild(newNotification);
 
@@ -27,7 +38,13 @@ function createNotification() {
   }, 3000)
 }
 
+// Random Notification Message Function 
 function getRandomNotification() {
   const randomIndex = Math.floor(Math.random() * messages.length);
   return messages[randomIndex];
+}
+// Random Notification Type Function
+function getRandomType() {
+  const randomIndex = Math.floor(Math.random() * types.length);
+  return types[randomIndex];
 }
