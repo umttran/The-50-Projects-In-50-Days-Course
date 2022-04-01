394ca10bf6912bb718e02f2eee846d694901913a
Umut Turan <umutturan107@gmail.com>
2022-04-01 19:21:56 +0300

    Add JS to fetch user data written into input tag

--- Day_28-Github-Profiles/script.js
+++ Day_28-Github-Profiles/script.js
@@ -1,6 +1,10 @@
 // The root Github URL
 const APIURL = 'https://api.github.com/users/';
 
+// Bring elements from the DOM
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 getUserInfo('umttran');
 
 async function getUserInfo(username){
@@ -12,5 +16,18 @@ async function getUserInfo(username){
   } catch (error) {
     console.log(error);
   }
-  
+
 }
+
+// Form Event Listener
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if(user) {
+    getUserInfo(user);
+
+    search.value = '';
+  }
+})
