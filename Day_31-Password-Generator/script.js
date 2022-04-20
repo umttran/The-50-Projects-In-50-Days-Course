542f6301755afb52b04c23fbe416b2c4cef944b3
Umut Turan <umutturan107@gmail.com>
2022-04-20 21:01:33 +0300

    Add JS to bring elements from the DOM

---
+++ Day_31-Password-Generator/script.js
@@ -0,0 +1,9 @@
+// Bring elements from the DOM
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
