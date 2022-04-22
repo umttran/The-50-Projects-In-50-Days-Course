d8e4f063b10de3896aa520259ab9ad1dd948f083
Umut Turan <umutturan107@gmail.com>
2022-04-22 20:07:05 +0300

    Add random character generation functions to JS

--- Day_31-Password-Generator/script.js
+++ Day_31-Password-Generator/script.js
@@ -1,9 +1,39 @@
 // Bring elements from the DOM
 const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
+const lengthEl = document.getElementById('pw-length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
+
+// Random Character Generation Functions
+// Lower Character
+function randomLowerChar() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+// Upper Character
+function randomUpperChar() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+// Number
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+// Symbols from specific string
+function randomSymbol() {
+  const symbols = '!&%.,:;_-@#+-/*=';
+
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+// Used method source:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+// Character set source:
+// https://www.w3schools.com/charsets/ref_html_ascii.asp
+
+// End - Random Character Generation Functions
