c65ce1ac1002a04e3e2547be3ef28c6339c3a6c8
Umut Turan <umutturan107@gmail.com>
2022-05-05 18:52:49 +0300

    Bring elements from the DOM

---
+++ Day_34-Animated-Countdown/script.js
@@ -0,0 +1,5 @@
+// Bring elements from the DOM
+const numbers = document.querySelectorAll('.numbers span');
+const counter = document.querySelector('.counter');
+const replay = document.querySelector('.replay');
+const replayBtn = document.getElementById('replay');
