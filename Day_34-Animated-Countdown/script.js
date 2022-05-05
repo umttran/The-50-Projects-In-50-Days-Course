cf957e361ab8eb879becc829511b90848f7aabb1
Umut Turan <umutturan107@gmail.com>
2022-05-05 20:14:41 +0300

    Add funcionality to animations

--- Day_34-Animated-Countdown/script.js
+++ Day_34-Animated-Countdown/script.js
@@ -3,3 +3,25 @@ const numbers = document.querySelectorAll('.numbers span');
 const counter = document.querySelector('.counter');
 const replay = document.querySelector('.replay');
 const replayBtn = document.getElementById('replay');
+
+runAnimation();
+
+function runAnimation() {
+  numbers.forEach( (number, index) => {
+    
+    const lastIndex = numbers.length - 1;
+
+    number.addEventListener('animationend', (e) => {
+      if(e.animationName === 'goIn' && index!== lastIndex){
+        number.classList.remove('in');
+        number.classList.add('out');
+      } else if (e.animationName === 'goOut' && number.nextElementSibling) {
+        number.nextElementSibling.classList.add('in');
+      }else {
+        counter.classList.add('hideEl')
+        replay.classList.add('showEl')
+      }
+    })
+
+  })
+}
