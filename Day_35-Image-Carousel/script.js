270720eeee9b00f870d3fcf22c8e0ec24d824fd2
Umut Turan <umutturan107@gmail.com>
2022-05-11 10:07:43 +0300

    Bring elements from the DOM

---
+++ Day_35-Image-Carousel/script.js
@@ -0,0 +1,6 @@
+// Bring elements from the DOM
+const images = document.getElementById('images');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+const img = document.querySelectorAll('#images img');
